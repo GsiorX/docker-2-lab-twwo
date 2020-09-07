c2b9180dcf97c68531fff8c88588eb93e6eff0ef
Jakub Gasiorek <220890@student.pwr.edu.pl>
2020-09-07 16:38:45 +0200

    Fix hardcoded secret

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import {AppService} from './app.service';
 import {UsersModule} from './users/users.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {JwtModule} from "@nestjs/jwt";
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
+import {AuthService} from './auth/auth.service';
+import {AuthModule} from './auth/auth.module';
 
 @Module({
-	imports: [TypeOrmModule.forRoot(), JwtModule.register({secretOrPrivateKey: 'secret123'}), UsersModule, AuthModule],
+	imports: [TypeOrmModule.forRoot(), JwtModule.register({secretOrPrivateKey: process.env.JWT_SECRET}), UsersModule, AuthModule],
 	controllers: [AppController],
 	providers: [AppService, AuthService],
 })
